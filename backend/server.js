e5035286f56444dc1f052c7aa6306c472db6fe98
vinay2005kumar <buttalavinay@gmail.com>
2025-05-17 19:52:23 +0530

    bakcend

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,9 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something broke!', error: err.message });
 });
-
+app.get('/',(req,res)=>{
+  res.send('working..')
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
